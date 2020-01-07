f93ba47558e18961aa3abd6d84d2de70aa09ec27
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-07 20:51:32 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+end
+
+def cobble(brand, condition)
+  @brand="Nike"
+  @condition="old"
+  shoe.select do {|@brand, @condition|}
+    shoe == repair
+  puts "cobble says that the shoe has been repaired"
+  puts "cobble makes the shoe's condition new"
+end
+  
+end
